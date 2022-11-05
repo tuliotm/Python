cff53c8442bfa9574a5a05c4cf062aa1cc195b9c
tuliotm <115123010+tuliotm@users.noreply.github.com>
2022-11-04 21:32:47 -0300

    Delete Python directory

--- Python/angulos_sen_cos_tan.py
+++
@@ -1,8 +0,0 @@
-import math
-angulo = float(input("Digite o angulo que voce deseja: "))
-seno = math.sin(math.radians(angulo))
-print(f"O angulo de {angulo} tem o SENo de {seno:.2f}")
-cosseno = math.cos(math.radians(angulo))
-print(f"O angulo de {angulo} tem o coesso de {cosseno:.2f}")
-tangente = math.tan(math.radians(angulo))
-print(f"O angulo de {angulo} tem a tangente de {tangente:.2f}")
--- Python/volume_esfera.py
+++
@@ -1,7 +0,0 @@
-#Calcule o volume de uma esfera de Raio R, rm que R é fornecido pelo usuário
-
-r = float(input("Digite o valor de R: "))
-
-v = (4*3.14*(r**3))/3
-
-print(f"O valor do volume da esfera é de {v:0.2f}")
